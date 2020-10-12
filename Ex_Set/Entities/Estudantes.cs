7b80c533e2083716343c89c000aea0540b8ed679
Naiara Rodrigues <naiapr@gmail.com>
2020-10-12 14:59:49 -0300

    Finalizado

---
+++ Ex_Set/Entities/Estudantes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex_Set.Entities
+{
+    class Estudantes
+    {
+        public int Codigo { get; set; }
+
+        public Estudantes()
+        {
+
+        }
+
+        public Estudantes(int codigo)
+        {
+            Codigo = codigo;
+        }
+
+        public override int GetHashCode()
+        {
+            return Codigo.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Estudantes))
+            {
+                return false;
+            }
+            Estudantes other = obj as Estudantes;
+            return Codigo.Equals(other.Codigo);
+        }
+    }
+
+}
---
+++ Ex_Set/Program.cs
@@ -0,0 +1,38 @@
+﻿using Ex_Set.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Ex_Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Estudantes> set = new HashSet<Estudantes>();
+
+            Console.Write("Quantos estudantes há no curso A: ");
+            int n1 = int.Parse(Console.ReadLine());
+            for (int i = 1; i<= n1; i++)
+            {
+                int codigoA=int.Parse(Console.ReadLine());
+                set.Add(new Estudantes { Codigo = codigoA });
+            }
+            Console.Write("Quantos estudantes há no curso B: ");
+            int n2 = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n2; i++)
+            {
+                int codigoB = int.Parse(Console.ReadLine());
+                set.Add(new Estudantes { Codigo = codigoB });
+            }
+            Console.Write("Quantos estudantes há no curso C: ");
+            int n3 = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n3; i++)
+            {
+                int codigoC = int.Parse(Console.ReadLine());
+                set.Add(new Estudantes { Codigo = codigoC });
+            }
+
+            Console.Write("Total de Estudantes: " + set.Count);
+        }
+    }
+}
